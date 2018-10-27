b7f5fbb5175d2521443c0822fa558948708551f7
iamani123 <44510754+iamani123@users.noreply.github.com>
2018-10-27 17:02:33 +0100

    SVM_DataLetters

---
+++ svm.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 
+
+import numpy as np
+from sklearn.cross_validation import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+
+clf=SVC(gamma='auto')
+X=np.genfromtxt('DataSetLetter.csv', delimiter = ',',usecols=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16))
+y=np.genfromtxt('DataSetLetter.csv', delimiter = ',', dtype='str',usecols=(0))
+acc =np.zeros((2,40))
+n=0
+for i in range(500,20001,500):
+    idx = np.random.randint(20000.0, size=i)
+    new_X=X[idx,:]
+    new_y=y[idx]
+    acc[0,n]=i
+    np.any(np.isnan(new_X))
+   # np.any(np.isnan(new_y))
+    np.all(np.isfinite(new_X))
+    #new_X[new_X.eq(np.inf).any(axis=1)]
+    #np.all(np.isfinite(new_y))
+    #new_X.as_matrix().astype(np.float)
+    X_train, X_test, y_train,y_test  = train_test_split(new_X,new_y,test_size=0.20)
+    svclassifier = SVC(kernel='rbf')
+    svclassifier.fit(X_train, y_train)
+    y_pred=svclassifier.predict(X_test)
+    score=0
+    
+    for j in range(len(y_test)):
+        if(y_test[j]==y_pred[j]):
+            score=score+1
+    #print(score,"/",len(y_test))
+    result=(score/len(y_test))*100       
+    print(i," ",result)
+    acc[1,n]=result;
+    n=n+1
+fig, ax = plt.subplots(figsize=(12, 8))
+#ax.bar(index, avgCost)
+ax.plot(acc[0,:], acc[1,:], 'r', label='Cost')
+#plt.xticks(index, cost[0,:], fontsize=5, rotation=30)
+ax.set_xlabel('Number Of Data', fontsize=5)
+ax.set_ylabel('Accuracy', fontsize=5)
+ax.legend(loc=2)
+fig.savefig('AccForSVMAlgo.png')
+#from sklearn.metrics import classification_report, confusion_matrix  
+#print(confusion_matrix(y_test,y_pred))  
+#print(classification_report(y_test,y_pred))  
