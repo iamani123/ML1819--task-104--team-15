e77540c9f6e279f26a31f071777d3ae3d153fcfa
spandey7117 <44511432+spandey7117@users.noreply.github.com>
2018-10-27 12:08:56 -0700

    Add files via upload
    with time parameter

--- KNNClassification.py
+++ KNNClassification.py
@@ -1,18 +1,23 @@
 import numpy as np
+import datetime
 from sklearn.cross_validation import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 X=np.genfromtxt('DataSetLetter.csv', delimiter = ',',usecols=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16))
 y=np.genfromtxt('DataSetLetter.csv', delimiter = ',', dtype='str',usecols=(0))
-acc =np.zeros((2,40))
+acc =np.zeros((3,40))
 n=0
 for i in range(500,20001,500):
-    idx = np.random.randint(20000, size=i)
+    starttime=datetime.datetime.now()
+    print("startTime",starttime)
+    idx = np.random.choice(range(20000), i, replace=False)
     new_X=X[idx,:]
     new_y=y[idx]
     acc[0,n]=i
     X_train, X_test, y_train,y_test  = train_test_split(new_X,new_y,test_size=.2, random_state=0)
-    classifier=KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
+    classifier=KNeighborsClassifier(n_neighbors=3,metric='minkowski',p=2)
+    #classifier=LogisticRegression(random_state=0)
     classifier.fit(X_train,y_train)
     y_pred=classifier.predict(X_test)
     score=0
@@ -24,6 +29,15 @@ for i in range(500,20001,500):
     result=(score/len(y_test))*100       
     print(i," ",result)
     acc[1,n]=result;
+   
+    endtime=datetime.datetime.now()
+    print("endTime",endtime)
+    timetake=str(endtime-starttime)
+    timetake=timetake[5:]
+    print("timetake",timetake)
+    acc[2,n]=float(timetake)
+    print("")
+    
     n=n+1
 fig, ax = plt.subplots(figsize=(12, 8))
 #ax.bar(index, avgCost)
@@ -33,3 +47,12 @@ ax.set_xlabel('Number Of Data', fontsize=5)
 ax.set_ylabel('Accuracy', fontsize=5)
 ax.legend(loc=2)
 fig.savefig('AccForKNNAlgo.png')
+
+fig1, ax1 = plt.subplots(figsize=(12, 8))
+#ax.bar(index, avgCost)
+ax1.plot(acc[0,:], acc[2,:], 'b', label='Cost')
+#plt.xticks(index, cost[0,:], fontsize=5, rotation=30)
+ax1.set_xlabel('Number Of Data', fontsize=5)
+ax1.set_ylabel('TimeTaken', fontsize=5)
+ax1.legend(loc=2)
+fig1.savefig('AccForKNNAlgo.png')
