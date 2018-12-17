dd9496768e45d940524d84a4d52659871b6a9b21
spandey7117 <44511432+spandey7117@users.noreply.github.com>
2018-12-17 17:56:05 +0000

    Add files via upload

---
+++ Phase-2/heatmap.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+data = pd.read_csv("DataSetLetter.csv")
+X = data.iloc[:,1:16]  #independent columns
+y = data.iloc[:,0]    #target column i.e price range
+#get correlations of each features in dataset
+corrmat = data.corr()
+top_corr_features = corrmat.index
+plt.figure(figsize=(16,16))
+#plot heat map
+g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
