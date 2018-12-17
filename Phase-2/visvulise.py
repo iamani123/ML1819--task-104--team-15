8d40850e8208a3404c96db580126adae43513912
spandey7117 <44511432+spandey7117@users.noreply.github.com>
2018-12-17 17:55:08 +0000

    Add files via upload

---
+++ Phase-2/visvulise.py
@@ -0,0 +1,112 @@
+import numpy as np
+from sklearn.cross_validation import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+data=np.genfromtxt('DataSetLetter.csv', delimiter = ',', dtype='str',usecols=(0))
+X=data
+count1 =0
+count2=0
+count3=0
+count4=0
+count5=0
+count6=0
+count7=0
+count8=0
+count9=0
+count10=0
+count11=0
+count12=0
+count13=0
+count14=0
+count15=0
+count16=0
+count17=0
+count18=0
+count19=0
+count20=0
+count21=0
+count22=0
+count23=0
+count24=0
+count25=0
+count26=0
+count27=0
+count28=0
+
+for i in range (len(X)):
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+    if(X[i]=='A' ):
+        count1=count1+1;
+    if(X[i]=='B'):
+        count2=count2+1;
+print("Count1= ",count1)
+print("Count2= ",count2)
+
+        
