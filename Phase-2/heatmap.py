d7b851058c593f6c8db3272460bf7699bd715349
iamani123 <iamani123@users.noreply.github.com>
2018-12-18 17:24:39 +0000

    Update heatmap.py

--- Phase-2/heatmap.py
+++ Phase-2/heatmap.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-data = pd.read_csv("DataSetLetter.csv")
+data = pd.read_csv("D:\\Subjects\\ir\\ML1819--task-104--team-15\\Phase-2\\updated_letter_dataset_standardise.csv")
 X = data.iloc[:,1:16]  #independent columns
 y = data.iloc[:,0]    #target column i.e price range
 #get correlations of each features in dataset
